96ee79aedc96eeefa26034020d38f039f2c2417f
Abisel19 <48061858+Abisel19@users.noreply.github.com>
2021-12-02 00:35:01 -0500

    v2

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -1,44 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { MapsService } from 'src/app/services/maps.service';
-import { HttpClient, HttpHeaderResponse, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaderResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.css'],
-  providers: [MapsService]
 })
 export class PrincipalComponent implements OnInit {
-  http_option={
-    headers: new HttpHeaders()
-  }
+  
 
-  constructor( public http: HttpClient) {
-
-    this.http_option.headers= new HttpHeaders({
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-    "Access-Control-Allow-Headers": "X-Requested-With, Content-Type, Authorization"
-    });
+  constructor() {
     
    }
+
+
   
 
   ngOnInit(): void {
-    // this.dataSvc.getAll().subscribe(res=>{
-    //   console.log('Res', res);
-    // });
-    this.obtener_ruta();
-  }
-
-  obtener_ruta(){
-    const heades = new Headers();
-    heades.append('Access-Control-Allow-Headers', 'Content-Type');
-    heades.append('Access-Control-Allow-Methods', 'GET');
-    heades.append('Access-Control-Allow-Origin', '*');
-     this.http.get('https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJmWs71vErTI8Rv7d4a3Auw2w&key=AIzaSyBWSSUndNg-8FWlbcWtK8WXuc48YpAfIvk', this.http_option)
-     .subscribe(data=>{console.log});
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJmWs71vErTI8Rv7d4a3Auw2w&key=AIzaSyBWSSUndNg-8FWlbcWtK8WXuc48YpAfIvk"; // site that doesn’t send Access-Control-*
+    fetch(proxyurl + url)
+    .then(response => response.json())
+    .then(contents => console.log(contents))
+    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
   }
 
 }
